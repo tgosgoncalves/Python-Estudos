8a3f7ea9f10c71f6bd5a8e81fedfa2a70b81a5cf
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-07-15 15:56:47 -0300

    add
    funções

--- Aula03 - dados numericos/app.py
+++ Aula03 - dados numericos/app.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+
+
 num1 = 5
 num2 = 3.5
 
@@ -7,10 +10,19 @@ b = int(num2)
 a = int(float("5.3"))
 b = "3.5"
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-print(num1 ** num2)
-print(num1 // num2)
+(num1 + num2)
+(num1 - num2)
+(num1 * num2)
+(num1 / num2)
+(num1 ** num2)
+(num1 // num2)
+
+print(abs(-15)) #ddddddddd# 
+print(pow(3,3)) 
+print(max(1,5))
+print(min(1,5))
+print(round(8.3))
+
+
+
 
