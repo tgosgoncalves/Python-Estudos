17d908123ad73ee8ed41c6db6338968d8564aaf2
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-07-15 15:18:23 -0300

    load
    load

---
+++ scripts - Python/ex002/exercicio002.py
@@ -0,0 +1,10 @@
+from tokenize import String
+nome = 'Tiago' 
+idade = '26' 
+gosta_de = 'skate'
+
+
+print(f'Meu nome é: {nome}')
+print(f'Eu gosto de: {gosta_de}')
+print(f'Eu tenho: {idade} anos')
+print(f'{nome} gosta de {gosta_de} e tem {idade} anos.')
