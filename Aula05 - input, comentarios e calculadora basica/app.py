ce9c1030eb9b42ac6240de319b0517c59e6705b9
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-07-20 14:18:28 -0300

    Add
    Input
    calculadora
    comentário

---
+++ Aula05 - input, comentarios e calculadora basica/app.py
@@ -0,0 +1,20 @@
+# comentario
+'''
+comentario menos recomendado
+'''
+
+# input de usuario
+# nome = input('Digite seu nome: ')
+#idade = int(input(f'Qual a sua idade {nome} ?'))
+
+#nascimento = 2022 - idade
+
+#print(f'{nome}, voce nasceu em: {nascimento}')
+
+# calculadora basica
+
+nun1 = int(input('Digite um numero: '))
+nun2 = int(input('Digite outro numero: '))
+resultado = nun1 + nun2
+
+print(f'O resultado é igual a: {resultado}')
