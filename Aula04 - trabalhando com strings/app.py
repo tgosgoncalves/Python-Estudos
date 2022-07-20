3ec71ad4e15965b3f89ba96d6468cd97ad64cf5d
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-07-20 12:33:45 -0300

    add
    Formatando textos nas strings

---
+++ Aula04 - trabalhando com strings/app.py
@@ -0,0 +1,28 @@
+# minha_string = 'qualquer texto'
+# print(f"concatenar {minha_string} em string") adicionando texto e string juntas#
+
+# print(minha_string.upper()) deixa tudo em maiusculo
+# print(minha_string.lower()) deixa tudo em minusculo
+# print(minha_string.capitalize()) deixa a primeira letra maiuscula
+# print(minha_string.strip()) remove espaçõs do inicio do texto (inicio e fim)
+# print(minha_string.replace('qualquer', 'meu')) substitui uma palavra, letrapor outra
+# print(len(minha_string)) mostra otamanho da string
+# print(minha_string.index('u')) procura a letra ou a palavra
+
+# texto com multiplas linhas usando aspas 
+
+# minha_string = '''linha 1,
+# linha 2,
+# linha 3.
+# ''' 
+# print(minha_string)
+
+# texto com multiplas linhas
+
+# minha_string = 'linha 1,\nlinha 2,\nlinha 3.'
+# print(minha_string)
+
+# colocando aspas sem fechar a string
+
+minha_string = ('linha 1, \"linha 2\", linha 3.')
+print(minha_string)
