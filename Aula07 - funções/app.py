1a45aa54a4a97e914500b535f64466e2f57ce348
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-08-03 00:47:13 -0300

    add
    Função de soma

--- Aula07 - funções/app.py
+++ Aula07 - funções/app.py
@@ -1,30 +1,55 @@
 #def big_mec():
+
 #    print('Sanduiche Big Mec')
+
 
 #big_mec()
 
+
 def cliente(nome1):
+
     print(f'Cliente: {nome1}')
 
+
 def fazer_big_mec(nome):
+
     print(f'Sanduiche: {nome}')
 
+
 def fritar_batata(tamanho):
+
     print(f'Batata: {tamanho}')
 
+
 def preparar_bebida(tipo,tamanho):
+
     print(f'Bebida: {tipo}, {tamanho}')
 
 
+
 #fazer_big_mec('Tiago')
+
 #fritar_batata('Grande')
+
 #preparar_bebida('Suco', 'Medio')
 
-def combo1(nome, tamanho_batata, tipo_bebida, tamanho_bebida):
+
+#def combo1(nome, tamanho_batata, tipo_bebida, tamanho_bebida):
+
 
     cliente('Tiago')
+
     fazer_big_mec('Big Mec')
+
     fritar_batata('Grande')
+
     preparar_bebida('Suco', 'grande')
 
-combo1('Tiago', 'Big Mec' 'Grande', 'Suco', 'grande')
+
+#combo1('Tiago', 'Big Mec' 'Grande', 'Suco', 'grande')
+
+def soma_num(num1, num2):
+    return num1 + num2
+
+resultado = soma_num(10,10)
+print(resultado)
