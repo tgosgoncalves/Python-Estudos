ddd5f9d97d7f73f3a7450bcda4d596a81125ab73
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-08-03 00:36:24 -0300

    add
    combo de funções

--- Aula07 - funções/app.py
+++ Aula07 - funções/app.py
@@ -6,18 +6,21 @@
 def fazer_big_mec(nome):
     print(f'Sanduiche: Big Mec {nome}')
 
-#fazer_big_mec('Tiago')
-#fazer_big_mec('Rogerio')
-#fazer_big_mec('Lucas')
-
 def fritar_batata(tamanho):
     print(f'Batata: {tamanho}')
 
 def preparar_bebida(tipo,tamanho):
     print(f'Bebida: {tipo}, {tamanho}')
 
+
 #fazer_big_mec('Tiago')
 #fritar_batata('Grande')
 #preparar_bebida('Suco', 'Medio')
 
-def 
+def combo1(nome, tamanho_batata, tipo_bebida, tamanho_bebida):
+
+    fazer_big_mec('Tiago')
+    fritar_batata('Grande')
+    preparar_bebida('Suco', 'grande')
+
+combo1('Tiago', 'Grande', 'Suco', 'grande')
