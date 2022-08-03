4929d55b5e02934dea7e8835a4eb130c44c8de55
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2022-08-02 23:59:47 -0300

    add
    Função + string

--- Aula07 - funções/app.py
+++ Aula07 - funções/app.py
@@ -4,8 +4,20 @@
 #big_mec()
 
 def fazer_big_mec(nome):
-    print(f'Sanduiche Big Mec {nome}')
+    print(f'Sanduiche: Big Mec {nome}')
 
-fazer_big_mec('Tiago')
-fazer_big_mec('Rogerio')
-fazer_big_mec('Lucas')
+#fazer_big_mec('Tiago')
+#fazer_big_mec('Rogerio')
+#fazer_big_mec('Lucas')
+
+def fritar_batata(tamanho):
+    print(f'Batata: {tamanho}')
+
+def preparar_bebida(tipo,tamanho):
+    print(f'Bebida: {tipo}, {tamanho}')
+
+#fazer_big_mec('Tiago')
+#fritar_batata('Grande')
+#preparar_bebida('Suco', 'Medio')
+
+def 
