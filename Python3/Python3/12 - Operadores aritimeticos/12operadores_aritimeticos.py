73a85c153f09aea6f793b3d350be7ac5fbcde929
Tiago Gonçalves <90118392+tgosgoncalves@users.noreply.github.com>
2023-01-16 21:53:38 -0300

    Add files via upload

---
+++ Python3/Python3/12 - Operadores aritimeticos/12operadores_aritimeticos.py
@@ -0,0 +1,28 @@
+# Operadores aritimeticos
+
+
+print('Multiplicação: 10 * 10 =', 10 * 10)
+print('Multiplicação por String: 10 * Tiago =', 10 * 'Tiago')  # Repedição da String 10 vezes
+
+
+print('Soma: 10 + 10 =', 10 + 10)
+print('Soma: 10 + Tiago =', 'Tiago' + 'Tiago')  # concatenação = TiagoTiago
+
+
+print('Subtração: 10 - 5 =', 10 - 5)
+
+print('Divisão: 50 / 10 =', 50 / 10 )
+
+print('Divisão: 10.5 // 3 =', 10.5 / 3)
+print('Divisão de inteiro: 10.5 // 3 =', 10.5 // 3)
+
+print('Precedencia')
+print(5+2*10)
+print((5+2)*10)
+
+
+
+
+
+
+
